0d4177d0a14052a869e0698496603d90ad03fba1
Shimon Richik <shimon@playboxtech.com>
2016-01-29 15:20:09 +0200

    basic segment building

---
+++ Assets/Scripts/PatternProxy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PatternProxy : MonoBehaviour {
+
+    public Segment SegmentPF;
+
+    public List<Vertex> Verteces;
+    public List<Segment> Segments;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CreatePattern() {
+
+        Segments.Add(Instantiate(SegmentPF));
+
+        Segments[0].VertexA = Verteces[0];
+        Segments[0].VertexB = Verteces[1];
+        Segments[0].Place();
+    }
+
+    [Serializable]
+    public class Vertex {
+        public Vector2 location;
+    }
+    
+}
---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Segment : MonoBehaviour {
+    
+    
+    public PatternProxy.Vertex VertexA;
+    public PatternProxy.Vertex VertexB;
+    
+
+    public void Place() {
+        transform.localPosition = VertexA.location;
+        transform.localRotation = Quaternion.LookRotation(VertexB.location - VertexA.location, Vector3.back);
+        transform.localScale = new Vector3(1, 1, (VertexB.location - VertexA.location).magnitude);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
